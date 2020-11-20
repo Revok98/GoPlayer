89b3c8b58715cbe27082aa9e1d340d5a6cf762b4
Revok98 <revok98@gmail.com>
2020-11-20 12:26:39 +0100

    début y_true

--- get-end-by-gnugo.py
+++ get-end-by-gnugo.py
@@ -1,5 +1,5 @@
 from GnuGo import *
-
+import numpy as np
 #board = Board()
 #board.prettyPrint()
 #for m in "D4 E4 D3 E5 E6 E7 D2 F6 G7 D6 B2 F5 B3 G5 C2 G6 C3".split():
@@ -10,7 +10,13 @@ from GnuGo import *
 
 #import sys
 #sys.exit()
+def name_to_coord(s):
+    assert s != "PASS"
+    indexLetters = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7, 'J':8}
 
+    col = indexLetters[s[0]]
+    lin = int(s[1:]) - 1
+    return col, lin
 def monte_carlo(gnugo, moves, nbsamples = 100):
 
     list_of_moves = [] # moves to backtrack
@@ -22,30 +28,49 @@ def monte_carlo(gnugo, moves, nbsamples = 100):
     white_wins = 0
     black_points = 0
     white_points = 0
+    nb_victoryB = np.zeros((9,9))
+    nb_victoryW = np.zeros((9,9))
+    nb_seen = np.zeros((9,9))
+    probaB = np.zeros((9,9))
+    probaW = np.zeros((9,9))
     while epochs < nbsamples:
         #print(epochs)
+        isfirstmove = True
         while True:
             m = moves.get_randomized_best()
             #print(epochs, m)
+            if isfirstmove:
+                firstmove = m
+                isfirstmove = False
             r = moves.playthis(m)
             if r == "RES":
                 return None
             list_of_moves.append(m)
             if len(list_of_moves) > 1 and list_of_moves[-1] == "PASS" and list_of_moves[-2] == "PASS":
                 break
+        if firstmove != "PASS":
+            nb_seen[name_to_coord(firstmove)] += 1
         score = gnugo.finalScore()
         toret.append(score)
         if score[0] == "W":
             white_wins += 1
+            if firstmove != "PASS":
+                nb_victoryW[name_to_coord(firstmove)] += 1
             if score[1] == "+":
                 white_points += float(score[2:])
         elif score[0] == "B":
             black_wins += 1
+            if firstmove != "PASS":
+                nb_victoryB[name_to_coord(firstmove)] += 1
             if score[1] == "+":
                 black_points += float(score[2:])
         (ok, res) = gnugo.query("gg-undo " + str(len(list_of_moves)))
         list_of_moves = []
         epochs += 1
+    probaB = np.divide(nb_victoryB, nb_seen,out=np.zeros_like(nb_victoryB), where=nb_seen!=0)
+    probaW = np.divide(nb_victoryW, nb_seen,out=np.zeros_like(nb_victoryW), where=nb_seen!=0)
+    print(probaB)
+    print(probaW)
     return epochs, toret, black_wins, white_wins, black_points, white_points
 
 def doit():
